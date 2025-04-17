62a26d130d2a20a1851db30db5c62369796aae3c
HabibaSanad <habibaabdelmalik477@gmail.com>
2025-04-17 03:15:36 +0200

    initial commit

---
+++ controller/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetSkills()
+        {
+            return View();
+        }
+
+        public IActionResult GetProjects()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About Me";
+            return View();
+        }
+
+        public IActionResult ContactMe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitContactForm(string name, string email, string message)
+        {
+            ViewBag.Message = $"Thank you for contacting us, {name}! We will get back to you shortly.";
+            return View("ContactMe");
+        }
+    }
+}
