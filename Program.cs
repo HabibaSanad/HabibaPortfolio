042befe590b515e8cc86b976fbafd592a71b8dd5
HabibaSanad <habibaabdelmalik477@gmail.com>
2025-04-17 03:26:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+// Program.cs
+namespace mvc1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run();
+        }
+    }
+}
